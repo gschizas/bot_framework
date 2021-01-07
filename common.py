041c833c9aa0de15d2bf4c92a5bf6ab6af9e62ae
George Schizas <gschizas@gmail.com>
2021-01-07 09:01:23 +0000

    Remove incomplete implementation of case insensitive dict

--- common.py
+++ common.py
@@ -97,10 +97,3 @@ def normalize_text(input_text):
     result = result.translate(tr)
     return result
 
-
-class CaseInsensitiveDict(dict):
-    def __setitem__(self, key, value):
-        super().__setitem__(key.lower(), value)
-
-    def __getitem__(self, key):
-        return super().__getitem__(key.lower())
